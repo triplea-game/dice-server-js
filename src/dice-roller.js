69ed35ecc5ff240be4f8ae4273d7f74e9f79e02d
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-11-19 17:05:56 +0100

    Use a true random function to generate dice

--- src/dice-roller.js
+++ src/dice-roller.js
@@ -1,10 +1,19 @@
 'use strict';
+const crypto = require('crypto');
 const roller = {};
 
+function getByteCount(max, times){
+	return Math.ceil(Math.log2(Math.pow(max, times)) / 8);
+}
+
 roller.roll = function(max, times){
+	const byteCount = getByteCount(max, times);
+	const randomToken = crypto.randomBytes(byteCount).readUIntBE(0, byteCount);
+	var remainingToken = randomToken;
 	const dice = [];
 	for(var i = 0; i < times; i++){
-		dice.push(Math.ceil(Math.random() * max));
+		dice.push(remainingToken % max);
+		remainingToken = Math.floor(remainingToken / max);
 	}
 	return dice;
 };
