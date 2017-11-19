c1e56940f0f747b29a29f7de9e5fbeaf97a52f3f
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-11-19 15:59:52 +0100

    Remove logger

--- src/logger.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const winston = require('winston');
-
-module.exports = winston.createLogger({
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.Console()
-  ]
-});
