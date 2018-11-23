45bbc581e7827786252310c3c7909074492aca13
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-11-23 19:56:08 +0100

    Replace async dependency with ES16

--- src/api/api.js
+++ src/api/api.js
@@ -2,155 +2,122 @@
 const roller = require('./dice-roller');
 const validator = require('./validator');
 const base64url = require('base64-url');
-const async = require('async');
 const emailManager = require('./email-manager.js');
 const dbhandler = require('./db-handler');
 
-const registrationMiddleware = (req, res, next) => {
+const registrationMiddleware = async (req, res, next) => {
 	const errors = [];
-	async.each([req.body.email1, req.body.email2], (email, callback) => {
-		dbhandler.checkMail(email, result => {
+	const checkPromises = [req.body.email1, req.body.email2].map((email) => (
+		new Promise((resolve) => dbhandler.checkMail(email, result => {
 			if(!result){
-				errors.push('Email "' + email +'" not registered.');
+				errors.push(`Email "${email}" not registered.`);
 			}
-			callback();
+			resolve();
+		}))
+	));
+	for (const promise in checkPromises) {
+		await promise;
+	}
+	if (errors.length > 0) {
+		res.status(403).json({
+			status: 'Error',
+			errors: errors
 		});
-	}, () => {
-		if(errors.length > 0){
-			res.status(403).json({
-				status: 'Error',
-				errors: errors
-			});
-		} else {
-			next();
-		}
-	});
+	} else {
+		next();
+	}
 }
 
-const handleRoll = (req, res) => {
-	async.waterfall([
-		callback => {
-			req.params.max = parseInt(req.params.max);
-			req.params.times = parseInt(req.params.times);
-			callback(null, req, res);
-		},
-		validateRollArgs,
-		callback => callback(null, req.params.max, req.params.times),
-		roller.roll,
-		(dice, callback) => {
+const handleRoll = async (req, res) => {
+		req.params.max = parseInt(req.params.max);
+		req.params.times = parseInt(req.params.times);
+		if (validateRollArgs(req, res)) {
+			const dice = await roller.roll(req.params.max, req.params.times);
 			const now = new Date();
-			async.waterfall([
-				callback => callback(pushToCopy(dice, now.getTime())),
-				validator.sign
-			], (error, signature) => {
-				res.json({
-					status: 'OK',
-					result: {
-						dice: dice,
-						signature: {
-							plain: signature,
-							urlescaped: base64url.escape(signature)
-						},
-						date: now.toISOString()
-					}
-				});
-			});
-			callback(null);
-		}]);
-}
-
-const validateRollArgs = (req, res, callback) => {
-	const errors = [];
-	async.each(['max', 'times'], (name, callback) => {
-		if(!req.params[name]){
-			errors.push(name + ' parameter is not defined');
-		} else if(!Number.isInteger(req.params[name])){
-			errors.push(name + ' parameter is not a valid number');
-		}
-		callback();
-	},
-	() => {
-		if(errors.length > 0){
-			res.status(422).json({
-				status: 'Error',
-				errors: errors
-			});
-			callback(errors);
-		} else {
-			callback(null);
-		}
-	});
-}
-
-const handleVerify = (req, res) => {
-	async.waterfall([
-		callback => {
-			req.params.signature = base64url.unescape(req.params.signature);
-			callback(null, req, res);
-		},
-		validateVerifyArgs,
-		callback => {
-			req.params.diceArray.push(new Date(req.params.date).getTime());
-			callback(null, req.params.diceArray, req.params.signature);
-		},
-		validator.verify,
-		(valid, callback) => {
+			const signature = validator.sign(pushToCopy(dice, now.getTime()));
 			res.json({
 				status: 'OK',
-				valid: valid
-			});
-			callback(null);
+				result: {
+					dice: dice,
+					signature: {
+						plain: signature,
+						urlescaped: base64url.escape(signature)
+					},
+					date: now.toISOString()
+				}
+			})
 		}
-	]);
 }
 
-const validateVerifyArgs = (req, res, callback) => {
+const validateRollArgs = (req, res) => {
 	const errors = [];
-	async.waterfall([
-		callback => {
-			try {
-				req.params.diceArray = JSON.parse(req.params.diceArray);
-			} catch(e){
-				if(!(e instanceof SyntaxError)){
-					return callback(e);
-				} else {
-					errors.push('The supplied diceArray parameter is invalid JSON.');
-				}
-			}
-			callback();
-		},
-		callback => {
-			if(errors.length === 0){
-				if(Array.isArray(req.params.diceArray)){
-					req.params.diceArray = req.params.diceArray.map(o => parseInt(o));
-					if(req.params.diceArray.some(Number.isNaN)){
-						errors.push('The provided diceArray parameter contains values other than integers.');
-					}
-				} else{
-					errors.push('The provided diceArray parameter is not an array.');
-				}
-			}
-			callback();
+	for (const name in ['max', 'times']) {
+		if (!req.params[name]) {
+			errors.push(name + ' parameter is not defined');
+		} else if (!Number.isInteger(req.params[name])) {
+			errors.push(name + ' parameter is not a valid number');
 		}
-	]);
-	if(req.params.signature.length !== 684){
-		errors.push('The provided signature has a wrong length.');
 	}
-	if(errors.length > 0){
+	if (errors.length > 0) {
 		res.status(422).json({
 			status: 'Error',
 			errors: errors
 		});
-		callback(errors);
-	} else {
-		callback(null);
+		return false;
+	}
+	return true;
+}
+
+const handleVerify = (req, res) => {
+	req.params.signature = base64url.unescape(req.params.signature);
+	if (validateVerifyArgs(req, res)) {
+		req.params.diceArray.push(new Date(req.params.date).getTime());
+		const valid = validator.verify(req.params.diceArray, req.params.signature);
+		res.json({
+			status: 'OK',
+			valid: valid
+		});
 	}
 }
 
+const validateVerifyArgs = (req, res) => {
+	const errors = [];
+	try {
+		req.params.diceArray = JSON.parse(req.params.diceArray);
+	} catch (e) {
+		if (e instanceof SyntaxError) {
+			errors.push('The supplied diceArray parameter is invalid JSON.');
+		} else {
+			throw e;
+		}
+	}
+	if (errors.length === 0) {
+		if (Array.isArray(req.params.diceArray)) {
+			req.params.diceArray = req.params.diceArray.map(o => parseInt(o));
+			if (req.params.diceArray.some(Number.isNaN)) {
+				errors.push('The provided diceArray parameter contains values other than integers.');
+			}
+		} else{
+			errors.push('The provided diceArray parameter is not an array.');
+		}
+	}
+	if (req.params.signature.length !== 684) {
+		errors.push('The provided signature has a wrong length.');
+	}
+	if (errors.length > 0) {
+		res.status(422).json({
+			status: 'Error',
+			errors: errors
+		});
+		return false;
+	}
+	return true;
+}
+
 const handleEmailRegister = (req, res) => {
-	if(req.body.token){
-		if(!emailManager.verifyEmail(req.params.email, base64url.unescape(req.params.token),
-			() => res.status(200).json({status: 'OK'}))){
+	if (req.body.token) {
+		if (!emailManager.verifyEmail(req.params.email, base64url.unescape(req.params.token),
+			() => res.status(200).json({status: 'OK'}))) {
 			res.status(403).json({
 				status: 'Error',
 				errors: 'invalid Token or mail.'
@@ -164,9 +131,9 @@ const handleEmailRegister = (req, res) => {
 
 const handleEmailUnregister = (req, res) => {
 	emailManager.unregisterEmail(req.body.email, rowCount => {
-		if(rowCount == 1){
+		if (rowCount == 1) {
 			res.status(200).json({status: 'OK'});
-		} else if(rowCount == 0){
+		} else if (rowCount == 0) {
 			res.status(412).json({
 				status: 'Error',
 				errors: ['Email "' + req.body.email + '" does not exist in the database.']
